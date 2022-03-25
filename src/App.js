4a00907ca78440d134a276199764621937599a90
ycl818 <kevinyichienlee@gmail.com>
2022-03-25 23:02:16 +0800

    ADD: header content

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import './App.css';
-import { useState } from 'react'
+import "./App.css";
+import { useState } from "react";
+import Header from "./components/Header";
 
 function App() {
-   const tasks = ['Eat dinner', 'Do landuary', 'go to the gym']
+  
+
   return (
+      <Header/>
    
-    <div className="App">
-      <h2 style={{color: 'white'}}>Todo LIST APP</h2>
-      <input type="text" style={{outline: 'none'}} />
-      <button>Add task</button>
-      {tasks.map(task => (
-        <h2>{task}</h2>
-      ))}
-    </div>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,69 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <Wrapper>
+      <HeaderItem>
+        <i className="fas fa-bars" />
+      </HeaderItem>
+
+      <HeaderItem>
+        <i className="fas fa-border-all" />
+        <span>Dashboard</span>
+      </HeaderItem>
+
+      <HeaderItem>
+        <i className="fas fa-images" />
+        <span>Collection</span>
+      </HeaderItem>
+
+      <PlaceHolder />
+
+      <HeaderItem>
+        <Profile>
+          <img src="../pikachu.jpg" />
+        </Profile>
+      </HeaderItem>
+    </Wrapper>
+  );
+};
+
+export default Header;
+
+const PlaceHolder = styled.div`
+  flex: 1;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  height: 70px;
+  align-items: center;
+  background-color: #20212d;
+  padding: 0 20px;
+  -webkit-box-shadow: 0px 4px 15px 0px #121212;
+  box-shadow: 0px 4px 15px 0px #121212;
+  position: sticky;
+  top: 0;
+`;
+const HeaderItem = styled.div`
+  color: #eee;
+  padding: 10px 16px;
+  border-radius: 4px;
+
+  span {
+    margin-left: 10px;
+    font-weight:500;
+  }
+
+  &:hover {
+    background-color: #18181f;
+    transition:0.3s;
+    cursor:pointer;
+  }
+`;
+const Profile = styled.div`
+  img {
+    height: 30px;
+    border-radius: 50%;
+  }
+`;
